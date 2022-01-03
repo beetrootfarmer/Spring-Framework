e877e2f48f5ae58c7619323b9508e032f029061d
beetrootfarmer <87971876+beetrootfarmer@users.noreply.github.com>
2022-01-03 13:10:51 +0900

    1103

---
+++ _003_BoardWeb_CouplingWeak_Interface/src/main/java/polymorphism/TVUser.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+public class TVUser {
+	public static void main(String[] args) {
+		TV tv = new LgTV();
+		tv.powerOn();
+		tv.volumeUp();
+		tv.volumeDown();
+		tv.powerOff();
+	}
+}
